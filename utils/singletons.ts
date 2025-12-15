c12ef22ff6a233fb870f06f2950c8242f9436261
Thomas Ryszkiewicz <tom.ryszkiewicz@gmail.com>
2025-12-15 11:52:18 -0500

    Updated prisma client version to major 7 from 6. Made necessary updates to configuration so that the program can run without unanticipated errors.

--- utils/singletons.ts
+++ utils/singletons.ts
@@ -1,10 +1,21 @@
 /** This file stores all singleton objects */
 
+import 'dotenv/config';
 import { PrismaClient } from "../generated/prisma";
+import { env } from "prisma/config";
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
+const adapter = new PrismaBetterSqlite3({
+  url: env('DATABASE_URL'),
+})
 /**
  * Prisma Client is a generated database ORM client that's tailored to your database schema.
  * Be sure to run `prisma generate` after any schema change.
  * Documentation: https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/instantiate-prisma-client
- */
-export const prisma = new PrismaClient();
+ * See also: https://www.prisma.io/docs/orm/reference/prisma-client-reference 
+*/
+export const prisma = new PrismaClient({
+    log: ['info'],
+    errorFormat: 'pretty',
+    adapter,
+ });
